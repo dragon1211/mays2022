d740d5221d6eec91e5daeeb9fa890dafdc8a5a52
dragon1211 <inddev0807@gmail.com>
2022-01-27 19:06:33 -0800

    fixed eng faq

--- page-faq-en.php
+++ page-faq-en.php
@@ -1,8 +1,8 @@
 <?php
-switchLanguage();
-get_header();
-the_post();
+get_header('eng');
 ?>
+<link rel="stylesheet" href="/css/faq.css">
+<link rel="stylesheet" href="/css/sp_faq.css" media="(max-width: 640px)">
 <div class="page-header" style="background-image: url(/images/faq/header.jpg)">
 		<h1><i>Q &amp; A</i><strong>FAQs</strong></h1>
 	</div><!-- .page-header -->
@@ -23,69 +23,140 @@ the_post();
 					</div>
 					<a class="qa_anc" name="qaanc01"></a>
 					<div id="qa_cat01" class="faq21c2">
-						<div class="faq21c21">How much would it cost to rent a whole set of furniture? We offer tailor-made interior plans based on your budget, desired product grade, and other requirements. Please see our sample plans based on a rough estimate or a price simulator.</div>
-						<div class="faq21c21">What durations are available? You can choose from 1, 3, 6, 9, 12, 24, and 36 months. For a longer period, you can renew the contract and continue to use our service. </div>
-						<div class="faq21c21">Are there any other fees at the time of signing the contract, apart from monthly lease payments? Initial costs include lease payments for two months and incidental charges for loading/unloading, installation, etc. In addition, we will conduct routine checks prior to the signing of a lease contract. As a result, we may require a deposit from you at the time of signing the contract.</div>
-						<div class="faq21c21">Can I take the leased furniture with me when I move? You are free to move the leased furniture yourself. However, you are requested to inform us in advance of your moving date and new address.</div>
-						<div class="faq21c21">Can I add an item to the contract? No, you need to sign a new contract for an additional item.</div>
-						<div class="faq21c21">Can I exchange products during the lease? As a general rule, we will not accept exchanges of products during the lease. However, if there is any defect on the product, we will replace or repair it within four years from the starting date of the contract.</div>
-						<div class="faq21c21">Can I choose the duration of lease when renewing the contract? You can choose from 1 to 12 months.</div>
-						<div class="faq21c21">If I renew the lease contract, will my monthly payments be cheaper than the original amount? For a long-term contract (12 months or more), the amount will decrease according to the post-renewal rate.</div>
-						<div class="faq21c21">I would like to know how much it will cost to buy the leased items when my contract expires. The buying rates vary depending on the initial contract duration, but generally 3–6% of the list price.</div>
-						<div class="faq21c21">Can I return some of the leased products during the lease? We accept returns of products that are no longer needed during the lease. However, you must pay the lease fee as we do not accept any cancellation of some of the leased products during the lease.</div>
-						<div class="faq21c21">Can I buy the leased products during the lease? You can buy the leased products after making the lease payments as per the initial contract. </div>
-						<div class="faq21c21">How much is the damage fee if I stain a product? You will be charged whichever is cheaper, the buying price or the actual cost of repair. If impossible to repair, you will be charged the buying price plus the disposal fee.</div>
+						<div class="faq21c21">
+							<p>How much would it cost to rent a whole set of furniture? </p>
+							<p>We offer tailor-made interior plans based on your budget, desired product grade, and other requirements. Please see our sample plans based on a rough estimate or a price simulator.</p>
+						</div>
+						<div class="faq21c21">
+							<p>What durations are available? </p>
+							<p>You can choose from 1, 3, 6, 9, 12, 24, and 36 months. For a longer period, you can renew the contract and continue to use our service. </p>
+						</div>
+						<div class="faq21c21">
+							<p>Are there any other fees at the time of signing the contract, apart from monthly lease payments?</p>
+							<p>Initial costs include lease payments for two months and incidental charges for loading/unloading, installation, etc. In addition, we will conduct routine checks prior to the signing of a lease contract. As a result, we may require a deposit from you at the time of signing the contract.</p>
+						</div>
+						<div class="faq21c21">
+							<p>Can I take the leased furniture with me when I move?</p>
+							<p> You are free to move the leased furniture yourself. However, you are requested to inform us in advance of your moving date and new address.</p>
+						</div>
+						<div class="faq21c21">
+							<p>Can I add an item to the contract?</p>
+							<p> No, you need to sign a new contract for an additional item.</p>
+						</div>
+						<div class="faq21c21">
+							<p>Can I exchange products during the lease?</p>
+							<p> As a general rule, we will not accept exchanges of products during the lease. However, if there is any defect on the product, we will replace or repair it within four years from the starting date of the contract.</p>
+						</div>
+						<div class="faq21c21">
+							<p>Can I choose the duration of lease when renewing the contract?</p>
+							<p> You can choose from 1 to 12 months.</p>
+						</div>
+						<div class="faq21c21">
+							<p>If I renew the lease contract, will my monthly payments be cheaper than the original amount?</p>
+							<p>For a long-term contract (12 months or more), the amount will decrease according to the post-renewal rate.</p>
+						</div>
+						<div class="faq21c21">
+							<p>I would like to know how much it will cost to buy the leased items when my contract expires.</p>
+							<p> The buying rates vary depending on the initial contract duration, but generally 3–6% of the list price.</p>
+						</div>
+						<div class="faq21c21">
+							<p>Can I return some of the leased products during the lease?</p>
+							<p> We accept returns of products that are no longer needed during the lease. However, you must pay the lease fee as we do not accept any cancellation of some of the leased products during the lease.</p>
+						</div>
+						<div class="faq21c21">
+							<p>Can I buy the leased products during the lease?</p>
+							<p> You can buy the leased products after making the lease payments as per the initial contract. </p>
+						</div>
+						<div class="faq21c21">
+							<p>How much is the damage fee if I stain a product?</p>
+							<p> You will be charged whichever is cheaper, the buying price or the actual cost of repair. If impossible to repair, you will be charged the buying price plus the disposal fee.</p>
+						</div>
 					</div>
 					<div id="qa_cat02" class="faq21c2">
-						<div class="faq21c21">How long does it take from my first inquiry to the delivery? The shortest time span is around a week. If you are interested in a brand-new product, it will vary depending on the stock available from each manufacturer. Please contact us for more information.</div>
 						<div class="faq21c21">
-							What is a rough estimate for a set of curtains? Can you just give me the measurements?  
-							The cost of curtains varies greatly depending on the window size and the number of windows. You can get a rough estimate through a price simulation or by contacting and sending us your floor plan and other necessary information. 
-							Unfortunately, we do not offer a measurement service on its own.
+							<p>How long does it take from my first inquiry to the delivery? </p>
+							<p>The shortest time span is around a week. If you are interested in a brand-new product, it will vary depending on the stock available from each manufacturer. Please contact us for more information.</p>
 						</div>
-						<div class="faq21c21">Can you prepare more than one interior plan for me?
-							 We initially propose one plan only per property. We can propose multiple plans separately in different styles for a fee of ¥30,000 per plan (excl. tax). To propose a plan that satisfies your needs, we would like you to see our examples of work and sample plans and tell us your budget and desired style.</div>
-						<div class="faq21c21">Can I give my furniture as a trade-in?
-							Yes, please inquire about the furniture trade-in campaign. However, we may not be able to offer trade-ins of furniture depending on its manufacturer/brand or state.</div>
+						<div class="faq21c21">
+							<p>What is a rough estimate for a set of curtains? Can you just give me the measurements?</p>
+							<p>The cost of curtains varies greatly depending on the window size and the number of windows. You can get a rough estimate through a price simulation or by contacting and sending us your floor plan and other necessary information. 
+							Unfortunately, we do not offer a measurement service on its own.</p>
+						</div>
+						<div class="faq21c21">
+							<p>Can you prepare more than one interior plan for me?</p>
+							<p>We initially propose one plan only per property. We can propose multiple plans separately in different styles for a fee of ¥30,000 per plan (excl. tax). To propose a plan that satisfies your needs, we would like you to see our examples of work and sample plans and tell us your budget and desired style.</p>
+						</div>
+						<div class="faq21c21">
+							<p>Can I give my furniture as a trade-in?</p>
+							<p>Yes, please inquire about the furniture trade-in campaign. However, we may not be able to offer trade-ins of furniture depending on its manufacturer/brand or state.</p>
+						</div>
 					</div>
 					<div id="qa_cat03" class="faq21c2">
-						<div class="faq21c21">Do you deliver items anywhere in Japan? 
-							We deliver nationwide, excluding Okinawa and remote islands. For a shipment of our in-stock items, you will be charged the actual shipping cost from Tokyo.</div>
-						<div class="faq21c21">Do you deliver on weekends and national holidays? 
-							Yes, but a separate weekend fee is required.</div>
-						<div class="faq21c21">How long does it take to deliver and install furniture? 
-							It depends on the number of products delivered and the type of property, e.g., detached house or apartment, but usually takes around 2.5 hours to deliver and install a set of furniture.</div>
-						<div class="faq21c21">Can I change my delivery date and time?
-							Please contact us at least three business days prior to the desired date. We will put you in if there is a slot available.</div>
-						<div class="faq21c21">Is it okay if I’m not there for delivery? 
-							Yes, if you can arrange the key for us. We will deliver and install items even if you’re not in.</div>
+						<div class="faq21c21">
+							<p>Do you deliver items anywhere in Japan? </p>
+							<p>We deliver nationwide, excluding Okinawa and remote islands. For a shipment of our in-stock items, you will be charged the actual shipping cost from Tokyo.</p>
+						</div>
+						<div class="faq21c21">
+							<p>Do you deliver on weekends and national holidays? </p>
+							<p>Yes, but a separate weekend fee is required.</p>
+						</div>
+						<div class="faq21c21">
+							<p>How long does it take to deliver and install furniture? </p>
+							<p>It depends on the number of products delivered and the type of property, e.g., detached house or apartment, but usually takes around 2.5 hours to deliver and install a set of furniture.</p>
+						</div>
+						<div class="faq21c21">
+							<p>Can I change my delivery date and time?</p>
+							<p>Please contact us at least three business days prior to the desired date. We will put you in if there is a slot available.</p>
+						</div>
+						<div class="faq21c21">
+							<p>Is it okay if I’m not there for delivery? </p>
+							<p>Yes, if you can arrange the key for us. We will deliver and install items even if you’re not in.</p>
+						</div>
 					</div>
 					<div id="qa_cat04" class="faq21c2">
-						<div class="faq21c21">What kind of products do you deal with?  
-							We cover a wide range of products, from home furniture and interior goods to linens, tableware, curtains, and home appliances. We cover a wide range of products, from home furniture and interior goods to linens, tableware, curtains, and home appliances. Please note that we do not offer PCs, cell phones, and the like. See the products page for details. <a href="https://www.mays.co.jp/service/product-brand/" target="_blank" rel="noopener noreferrer"></div>
-						<div class="faq21c21">What kind of in-stock products do you have? Can I find them online?  
-							You can check in-stock products on our product management system. To do so, please visit our showroom or have an online interior consultation with our staff.
+						<div class="faq21c21">
+							<p>What kind of products do you deal with?</p>
+							<p>We cover a wide range of products, from home furniture and interior goods to linens, tableware, curtains, and home appliances. We cover a wide range of products, from home furniture and interior goods to linens, tableware, curtains, and home appliances. Please note that we do not offer PCs, cell phones, and the like. See the products page for details. <a href="https://www.mays.co.jp/service/product-brand/" target="_blank" rel="noopener noreferrer"></a></p>
 						</div>
-						<div class="faq21c21">Do you disinfect used products?
-							Yes, our warehouse staff members carefully maintain them.</div>
-						<div class="faq21c21">Do you clean the leased linens during the lease? 
-							No, the customer is responsible for cleaning during the lease.</div>
-						<div class="faq21c21">Do you replace the light bulbs in the leased lighting equipment? 
-							We do no supply any consumable goods during the lease.</div>
+						<div class="faq21c21">
+							<p>What kind of in-stock products do you have? Can I find them online?<p>
+							<p>You can check in-stock products on our product management system. To do so, please visit our showroom or have an online interior consultation with our staff.</p>
+						</div>
+						<div class="faq21c21">
+							<p>Do you disinfect used products?</p>
+							<p>Yes, our warehouse staff members carefully maintain them.</p>
+						</div>
+						<div class="faq21c21">
+							<p>Do you clean the leased linens during the lease? </p>
+							<p>No, the customer is responsible for cleaning during the lease.</p>
+						</div>
+						<div class="faq21c21">
+							<p>Do you replace the light bulbs in the leased lighting equipment? </p>
+							<p>We do no supply any consumable goods during the lease.</p>
+						</div>
 					</div>
 					<div id="qa_cat05" class="faq21c2">
-						<div class="faq21c21">Can I pay by credit card? 
-							Yes, we accept VISA, JCB, AMEX, etc.</div>
-						<div class="faq21c21">Can you issue one invoice for multiple leases?
-							Yes, please inform a member of our staff of your request.</div>
-						<div class="faq21c21">Can you invoice a different recipient for each property? 
-							Yes, we can be flexible about your request. Please contact a member of our staff.</div>
-						<div class="faq21c21">Can you send invoices or other documents to an address or a recipient that differs from the lease signer or billing address? 
-							Yes, please inform a member of our staff of your request. We mail monthly invoices, etc. to the specified recipient.</div>
+						<div class="faq21c21">
+							<p>Can I pay by credit card? </p>
+							<p>Yes, we accept VISA, JCB, AMEX, etc.</p>
+						</div>
+						<div class="faq21c21">
+							<p>Can you issue one invoice for multiple leases?</p>
+							<p>Yes, please inform a member of our staff of your request.</p>
+						</div>
+						<div class="faq21c21">
+							<p>Can you invoice a different recipient for each property? </p>
+							<p>Yes, we can be flexible about your request. Please contact a member of our staff.</p>
+						</div>
+						<div class="faq21c21">
+							<p>Can you send invoices or other documents to an address or a recipient that differs from the lease signer or billing address? </p>
+							<p>Yes, please inform a member of our staff of your request. We mail monthly invoices, etc. to the specified recipient.</p>
+						</div>
 					</div>
 					<div id="qa_cat06" class="faq21c2">
-						<div class="faq21c21">Can you mount a TV on the wall?
-							It is possible if you buy a TV from us (not leased items). However, if you live in a rental property, you will need to obtain the consent of the property owner and restore the wall to its original state when you move out. In addition, when mounting a heavy object such as an LCD TV on a wall surface, it is necessary to reinforce the wall by installing some backing boards beforehand. Customers are requested to prepare this in advance.
+						<div class="faq21c21">
+							<p>Can you mount a TV on the wall? </p>
+							<p>It is possible if you buy a TV from us (not leased items). However, if you live in a rental property, you will need to obtain the consent of the property owner and restore the wall to its original state when you move out. In addition, when mounting a heavy object such as an LCD TV on a wall surface, it is necessary to reinforce the wall by installing some backing boards beforehand. Customers are requested to prepare this in advance. </p>
 						</div>
 					</div>
 				</div>
@@ -95,20 +166,18 @@ the_post();
 						 <button id="qa_catmenu08">Change/cancellation of lease contracts</button> 
 						 <button id="qa_catmenu09">Other</button>
 					</div>
-					<!-- <a class="qa_anc" name="qaanc02"></a>
-					<div id="qa_cat07" class="faq21c2">
-						<div class="faq21c21">商品に不具合がある場合どうしたらよいですか？ 当社担当者までご連絡ください。その際、家具においては不具合部分の写真、家電等で動作に不具合がある場合はスマートフォンで撮影したビデオ等をお送りいただけますとスムーズです。</div>
-						<div class="faq21c21">商品の使い方・不具合に関するご質問 商品の使い方・不具合に関する回答が入ります。商品の使い方・不具合に関する回答が入ります。商品の使い方・不具合に関する回答が入ります。商品の使い方・不具合に関する回答が入ります。商品の使い方・不具合に関する回答が入ります。商品の使い方・不具合に関する回答が入ります。商品の使い方・不具合に関する回答が入ります。商品の使い方・不具合に関する回答が入ります。商品の使い方・不具合に関する回答が入ります。商品の使い方・不具合に関する回答が入ります。</div>
-					</div>
-					<div id="qa_cat08" class="faq21c2">
-						<div class="faq21c21">リース契約の変更・解約に関するご質問 リース契約の変更・解約に関する回答が入ります。リース契約の変更・解約に関する回答が入ります。リース契約の変更・解約に関する回答が入ります。リース契約の変更・解約に関する回答が入ります。リース契約の変更・解約に関する回答が入ります。リース契約の変更・解約に関する回答が入ります。リース契約の変更・解約に関する回答が入ります。リース契約の変更・解約に関する回答が入ります。リース契約の変更・解約に関する回答が入ります。リース契約の変更・解約に関する回答が入ります。</div>
-					</div>
-					<div id="qa_cat09" class="faq21c2">
-						<div class="faq21c21">その他ご質問 その他ご質問に関する回答が入ります。その他ご質問に関する回答が入ります。その他ご質問に関する回答が入ります。その他ご質問に関する回答が入ります。その他ご質問に関する回答が入ります。その他ご質問に関する回答が入ります。その他ご質問に関する回答が入ります。その他ご質問に関する回答が入ります。その他ご質問に関する回答が入ります。その他ご質問に関する回答が入ります。</div>
-					</div> -->
 				</div>
 			</div>
 			<!-- /wp:html -->
 		</section>
 	</div>
-<?php get_footer(); ?>
+	<script>
+		$('#qa_catmenu01').click(()=>SelectQA(1));
+		$('#qa_catmenu02').click(()=>SelectQA(2));
+		$('#qa_catmenu03').click(()=>SelectQA(3));
+		$('#qa_catmenu04').click(()=>SelectQA(4));
+		$('#qa_catmenu05').click(()=>SelectQA(5));
+		$('#qa_catmenu06').click(()=>SelectQA(6));
+	</script>
+	<script type="text/javascript" src="/js/faq.js"></script>
+<?php get_footer('eng'); ?>
